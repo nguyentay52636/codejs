5ed84441888ee9660e5923b5287bf49ec4b80493
tay52636 <phuongtay52636@gmail.com>
2023-09-08 12:21:55 +0700

    update

--- BT_buoi_18_js/main.js
+++ BT_buoi_18_js/main.js
@@ -1,14 +1,14 @@
 var arrays = [];
-
+var number;
 var number1;
 var number2;
 function themso() {
-  var number = document.getElementById("txtNumber").value * 1;
+  number = document.getElementById("txtNumber").value * 1;
   arrays[arrays.length] = number;
   document.getElementById("inra").innerHTML = arrays;
 }
 
-function bai1() {
+document.getElementById("tinhtongduong").onclick = function () {
   // var arrays = new Array ;
   var tong = 0;
   for (var i = 0; i < arrays.length; i++) {
@@ -16,11 +16,11 @@ function bai1() {
       tong += arrays[i];
     }
   }
-}
-themso();
-/*
+  document.querySelector("span").innerHTML = tong;
+};
+
 //bai2 dem so nguyen duong trong mang
-function bai2(arrays) {
+function bai2() {
   // var arrays = new Array ;
   var dem = 0;
   for (var i = 0; i < arrays.length; i++) {
@@ -28,7 +28,7 @@ function bai2(arrays) {
       dem++;
     }
   }
-  return dem;
+  document.getElementById("demsoduong").innerHTML = dem;
 }
 
 //tim min mang
@@ -40,21 +40,22 @@ function checkmin() {
       min_value = arrays[i];
     }
   }
-  return min_value;
+  document.getElementById("sonhonhat").innerHTML = min_value;
 }
 
 //bai 4 : tim so duong nho nhat
-function checkmin1() {
-  var min_value = arrays[0];
+function checkmin11() {
+  var min_value1 = arrays[0];
   for (var i = 0; i < arrays.length; i++) {
     if (arrays[i] > 0) {
-      if (arrays[i] < min_value) {
-        min_value = arrays[i];
+      if (arrays[i] < min_value1) {
+        min_value1 = arrays[i];
       }
     }
   }
-  return min_value;
+  document.getElementById("soduongmin").innerHTML = min_value1;
 }
+
 //bai5 Tìm số chẵn cuối cùng trong mảng. Nếu mảng không có giá trị chẵn thì trả về -1.
 function bai5() {
   var last_even = -1;
@@ -64,17 +65,13 @@ function bai5() {
       break;
     }
   }
-  return last_even;
+  document.getElementById("sochancuoi").innerHTML = last_even;
 }
-
+/*
 //bai 6
 function bai6() {
   if (
-    number1 >= 0 ||
-    number >= arrays.length ||
-    number2 < 0 ||
-    number2 >= arrays.length
-  ) {
+    number1 >= 0 || number >= arrays.length ||  number2 < 0 || number2 >= arrays.length) {
     alert("error! ");
   }
   var tmp = arrays[number1];
@@ -84,12 +81,12 @@ function bai6() {
 const number1 = parseInt(prompt("input number 1 : "));
 const number2 = parseInt(prompt("input number 2 : "));
 bai6(arrays, number1, number2);
-
+*/
 //bai 7
 function bai7() {
   var n = arrays.length;
-  for (var i = 0; i < n; i++) {
-    for (var j = i + 1; j < n - 1; j++) {
+  for (var i = 0; i < n - 1; i++) {
+    for (var j = i + 1; j < n; j++) {
       if (arrays[i] > arrays[j]) {
         var tmp = arrays[i];
         arrays[i] = arrays[j];
@@ -97,9 +94,41 @@ function bai7() {
       }
     }
   }
+  var result = "Mảng sau khi sắp xếp là : " + arrays;
+
+  document.getElementById("sapxepmang").innerHTML = result;
 }
 
 //bai8
+
+// function bai8() {
+//   for (var i = 0; i < arrays.length; i++) {
+//     if (arrays[i] < arrays[i + 1] || arrays[isprime]) {
+//       arrays[i + 1] = arrays[i];
+//       return true;
+//     }
+//   }
+//   return -1;
+// }
+function bai8() {
+  if (arrays.length === 0) {
+    alert("vui lòng nhập giá trị cho mảng ! ");
+    return;
+  }
+  var first_isprime = null;
+  for (var i = 0; i < arrays.length; i++) {
+    if (isprime(arrays[i])) {
+      if (first_isprime === null || arrays[i] < first_isprime) {
+      }
+    }
+  }
+  if (first_isprime === null) {
+    alert(" Không có số nguyên tố nào trong mảng ! ");
+  } else {
+    var result = "Số nguyên tố đầu tiên là : " + first_isprime;
+    document.getElementById("ktsont").innerHTML = result;
+  }
+}
 function isprime() {
   if (number < 2) {
     return false;
@@ -111,25 +140,17 @@ function isprime() {
   }
   return true;
 }
-function bai8() {
-  for (var i = 0; i < arrays.length; i++) {
-    if (arrays[i] < arrays[i + 1] || arrays[isprime]) {
-      arrays[i + 1] = arrays[i];
-      return true;
-    }
-  }
-  return -1;
-}
+
 //bai 9: Nhập thêm 1 mảng số thực, tìm xem trong mảng có bao nhiêu số nguyên?
 function count_interger() {
   var count = 0;
   var n = arrays.length;
   for (var i = 0; i < n; i++) {
-    if (Number.isInteger(arrays)) {
+    if (Number.isInteger(arrays[i])) {
       count++;
     }
   }
-  return count;
+  document.getElementById("demsonguyen").innerHTML = count;
 }
 
 //bai 10. So sánh số lượng số dương và số lượng số âm xem số nào nhiều hơn.
@@ -153,15 +174,19 @@ function count_soam() {
   return count_am;
 }
 function bai10() {
-  let dem1 = count_soduong(arrays);
-  let dem2 = count_soam(arrays);
+  if (arrays.length === 0) {
+    alert("vui lòng nhập giá trị cho mảng ! ");
+    return;
+  }
+  var dem1 = count_soduong(arrays);
+  var dem2 = count_soam(arrays);
   if (dem1 > dem2) {
-    console.log("so duong nhieu hon so am ");
-  } else if (dem2 < dem1) {
-    console.log("so am nhieu hon so duong ");
+    document.getElementById("sosanh").innerHTML = "Số âm" + "<" + "Số dương";
   } else {
-    console.log("2 so bang nhau ");
+    document.getElementById("sosanh").innerHTML = "Số dương" + "<" + "Số âm";
+  }
+  if (dem1 === dem2) {
+    document.getElementById("sosanh").innerHTML = "Số âm " + "=" + "Số dương ";
   }
 }
 // function themso() ;
-*/
